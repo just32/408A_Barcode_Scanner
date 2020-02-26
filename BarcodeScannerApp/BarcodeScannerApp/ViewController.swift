d6db63583d6b453d6b1e02d69c064aedc81d242e
Justin Frankle <jfrnkle32@gmail.com>
2020-02-26 18:46:22 -0500

    Initial Commit

---
+++ BarcodeScannerApp/BarcodeScannerApp/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  BarcodeScannerApp
+//
+//  Created by Justin Frankle on 2/26/20.
+//  Copyright © 2020 Justin Frankle. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
